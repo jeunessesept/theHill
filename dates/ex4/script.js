50e5b57f3599ec21edb604b19728bc7c7d10cc1b
jeunessesept <b.jiacinto@gmail.com>
2022-10-26 18:47:12 +0200

    processing

--- dates/ex4/script.js
+++ dates/ex4/script.js
@@ -1,24 +1,38 @@
-const dateOfday = new Date()
-const day = dateOfday.getDay()
-const year = dateOfday.getFullYear()
-const month = dateOfday.getMonth();
-const hoursDay = dateOfday.getHours();
-const minutesDay = dateOfday.getMinutes();
-const secondesDay = dateOfday.getSeconds()
-
-const weekday = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
-const months = ["Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
 const displayHours = () => {
-    const hoursOfToday = hoursDay + ": " + minutesDay + ": " + secondesDay;
+  const dateOfday = new Date();
+  let day = dateOfday.getDay();
+  let year = dateOfday.getFullYear();
+  let month = dateOfday.getMonth();
+  let hoursDay = dateOfday.getHours();
+  let minutesDay = dateOfday.getMinutes();
+  let secondesDay = dateOfday.getSeconds();
 
-    document.querySelector(".hours").innerHTML = hoursOfToday;
-    document.querySelector(".year").innerHTML = year;
-    document.querySelector(".day").innerHTML = weekday[dateOfday.getDay()];
-    document.querySelector(".pday").innerHTML = day;
-    document.querySelector(".month").innerHTML = months[dateOfday.getMonth()];
-}
+  const weekday = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-setInterval(displayHours, 1000)
+  let hoursOfToday = hoursDay + ": " + minutesDay + ": " + secondesDay;
 
-//const hoursOfToday = hoursDay + ": " + minutesDay + ": " + secondesDay;
+
+
+  document.getElementById("hours").innerHTML = hoursOfToday;
+  document.querySelector(".year").innerHTML = year;
+  document.querySelector(".day").innerHTML = weekday[dateOfday.getDay()];
+  document.querySelector(".pday").innerHTML = day;
+  document.querySelector(".month").innerHTML = months[dateOfday.getMonth()];
+
+};
+
+setInterval(displayHours, 1000);

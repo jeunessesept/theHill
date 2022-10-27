ad4abbd47daf3f2b5649d88f50f06df41a827515
jeunessesept <b.jiacinto@gmail.com>
2022-10-27 09:47:21 +0200

    nearby end

--- dates/ex4/script.js
+++ dates/ex4/script.js
@@ -1,38 +1,42 @@
 const displayHours = () => {
   const dateOfday = new Date();
-  let day = dateOfday.getDay();
+  let day = dateOfday.getDate();
   let year = dateOfday.getFullYear();
   let month = dateOfday.getMonth();
   let hoursDay = dateOfday.getHours();
   let minutesDay = dateOfday.getMinutes();
   let secondesDay = dateOfday.getSeconds();
+  
 
-  const weekday = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
   ];
 
+
+
+  hoursDay = hoursDay < 10 ? "0" + hoursDay : hoursDay;
+  minutesDay = minutesDay < 10 ? "0" + minutesDay : minutesDay;
+  secondesDay = secondesDay < 10 ? "0" + secondesDay : secondesDay;
+
   let hoursOfToday = hoursDay + ": " + minutesDay + ": " + secondesDay;
-
-
 
   document.getElementById("hours").innerHTML = hoursOfToday;
   document.querySelector(".year").innerHTML = year;
   document.querySelector(".day").innerHTML = weekday[dateOfday.getDay()];
   document.querySelector(".pday").innerHTML = day;
   document.querySelector(".month").innerHTML = months[dateOfday.getMonth()];
-
 };
 
 setInterval(displayHours, 1000);

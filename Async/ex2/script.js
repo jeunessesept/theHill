c84260509b7e3894a9e9afc10133f29d230caa5b
jeunessesept <b.jiacinto@gmail.com>
2022-10-30 18:13:59 +0100

    processing

--- Async/ex2/script.js
+++ Async/ex2/script.js
@@ -2,51 +2,32 @@ const inputedName = document.getElementById("name");
 const inputContainer = document.querySelector(".container");
 const selectCountry = document.getElementById("countries");
 
-
-
 const button = document.querySelector(".api");
 
 const fetchApi = () => {
+  const country_id = document.getElementById("countries").value;
+
   button.addEventListener("click", () => {
-    const fetchName = (name) =>
-      fetch("https://api.agify.io/?name=" + name);
+    const fetchName = (name, country_id) =>
+      fetch(`https://api.agify.io/?name=${name}&country_id=` + country_id);
 
-    inName = inputedName.value;
+    let inName = inputedName.value;
+    let countryChoosed = selectCountry.value;
 
-    fetchName(inName)
+    fetchName(inName, countryChoosed)
       .then((response) => response.json())
       .then((json) => {
         if (inName != "") {
           let inputeded = document.createElement("div");
           inputeded.innerHTML = inName + " is " + json.age + " years old";
           inputContainer.append(inputeded);
-          console.log(json.count);
+          
         }
       })
       .catch((error) => {
         console.log("There was an error!", error);
       });
   });
-  
-  selectCountry.addEventListener("change", () => {
-    const fetchCountry = (country_id) => 
-    fetchCountry("https://api.agify.io/?name=michael&country_id=" + country_id);
-
-    let countryChoosed = selectCountry.value;
-
-    fetchCountry(countryChoosed)
-    .then((res) => res.json())
-    .then (() => {
-      if (countryChoosed.value === "Belgium") {
-        country_id = "BE";
-      } else if (countryChoosed.value === "France") {
-        country_id = "FR";
-      } else if (countryChoosed.value === "Italy") {
-        country_id = "IT";
-      }
-    })
-  })
 };
 
 fetchApi();
-
